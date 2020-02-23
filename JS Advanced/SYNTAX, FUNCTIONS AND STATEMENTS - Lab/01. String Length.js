3f2ab0ce4397b47627e57a5bef599e4acd713867
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-02-23 18:57:15 +0200

    Create 01. String Length.js

---
+++ JS Advanced/SYNTAX, FUNCTIONS AND STATEMENTS - Lab/01. String Length.js
@@ -0,0 +1,12 @@
+function solve(arr1, arr2, arr3) {
+
+    l1 = arr1.length;
+    l2 = arr2.length;
+    l3 = arr3.length;
+
+    let sumLength = l1 + l2 + l3;
+    let average = Math.floor(sumLength / 3);
+
+    console.log(sumLength);
+    console.log(average);
+}
