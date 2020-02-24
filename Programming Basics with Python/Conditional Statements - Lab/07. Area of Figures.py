c108f5dc333f5ddbfcdb7a2c6614b7f046a3f8cf
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-02-24 13:29:39 +0200

    Update 07. Area of Figures.py

--- Programming Basics with Python/Conditional Statements - Lab/07. Area of Figures.py
+++ Programming Basics with Python/Conditional Statements - Lab/07. Area of Figures.py
@@ -5,16 +5,16 @@ dimension = float(input())
 
 if figure == "square":
     area = dimension * dimension
-    print (f'{area:.3f}')
+    
 elif figure == "rectangle":
     sideB = float(input())
     area = dimension * sideB
-    print(f'{area:.3f}')
+    
 elif figure == "circle":
     area = pi * dimension ** 2
-    print(f'{area:.3f}')
+    
 elif figure == "triangle":
     hight = float(input())
     area = dimension * hight / 2
-    print(f'{area:.3f}')
-
+    
+print (f'{area:.3f}')
