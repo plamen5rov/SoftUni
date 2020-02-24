a6917157cb7e2c6f3c643910cd29ba803ce24f85
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-02-24 13:23:17 +0200

    Create 04. Number 1...9 to Text.py

---
+++ Programming Basics with Python/Conditional Statements - Lab/04. Number 1...9 to Text.py
@@ -0,0 +1,22 @@
+number = int(input())
+
+if number == 1:
+    print("one")
+if number == 2:
+        print("two")
+if number == 3:
+    print("three")
+if number == 4:
+    print("four")
+if number == 5:
+    print("five")
+if number == 6:
+    print("six")
+if number == 7:
+    print("seven")
+if number == 8:
+    print("eight")
+if number == 9:
+    print("nine")
+if number > 9:
+    print("number too big")
