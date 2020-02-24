9a4960ca83f82f0cbd9ffaef944fbd06ae4b5ef4
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-02-24 14:06:05 +0200

    Create 08. Number sequence.py

---
+++ Programming Basics with Python/Loops - Lab/08. Number sequence.py
@@ -0,0 +1,23 @@
+import sys
+
+numbers_cont = int(input())
+
+max_number = -sys.maxsize
+
+min_number = sys.maxsize
+
+for counter in range(numbers_cont):
+
+   current_num = int(input())
+
+   if current_num > max_number:
+       max_number = current_num
+   if current_num < min_number:
+       min_number = current_num
+
+
+
+print(f'Max number: {max_number}')
+
+print(f'Min number: {min_number}')
+
