b805af19153a4f98b3b4b57fd82536a630128635
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-02-25 11:24:13 +0200

    Update 01. Old Books.py

--- Programming Basics with Python/Loops - Part 2 - Exercise/01. Old Books.py
+++ Programming Basics with Python/Loops - Part 2 - Exercise/01. Old Books.py
@@ -4,14 +4,17 @@ library_books = int(input())
 
 counter = 0
 
+book_found = False
+
 while counter < library_books:
     current_book = input()
     counter += 1
     if current_book == target_book:
+        book_found = True
         print(f'You checked {counter - 1} books and found it.')
         break
 
 
-if counter == library_books:
+if not book_found:
     print('The book you search is not here!')
     print(f'You checked {counter} books.')
