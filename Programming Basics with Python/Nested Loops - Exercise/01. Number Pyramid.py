31db4f7dd004756cc218881edab22a3c5c903a17
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-02-26 13:07:40 +0200

    Create 01. Number Pyramid.py

---
+++ Programming Basics with Python/Nested Loops - Exercise/01. Number Pyramid.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+number = 1
+
+cols = 1
+
+while number <= n:
+    
+    for i in range(0, cols):
+        if number > n:
+            break
+        print(number, end=' ')
+        number += 1
+    print()
+    cols += 1
