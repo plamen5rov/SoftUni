692e44911b22e77cba08eb1c2a5e158790f40f83
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-02-28 10:47:53 +0200

    Update 01. Savings.py

--- Programming Basics with Python/Exam Preparation/01. Savings.py
+++ Programming Basics with Python/Exam Preparation/01. Savings.py
@@ -1,1 +1,17 @@
+monthly_income = float(input())
 
+months = int(input())
+
+expenses = float(input())
+
+reserve = monthly_income * 0.30
+
+money_left = monthly_income - expenses - reserve
+
+saved_money = months * money_left
+
+percentage = money_left / monthly_income * 100
+
+print(f'She can save {percentage:.2f}%')
+
+print(f'{saved_money:.2f}')
