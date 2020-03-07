f5f1e2abb428905085cee2ef8a65b54557dd2d5e
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-03-07 12:32:05 +0200

    Create 02. Greatest Common Divisor – GCD.js

---
+++ JS Advanced/EXERCISE: SYNTAX, FUNCTIONS AND STATEMENTS/02. Greatest Common Divisor – GCD.js
@@ -0,0 +1,18 @@
+function gcd(a, b) {
+
+    let copyA = a;
+    let copyB = b;
+
+    while (copyB !== 0) {
+
+        let tempMod = copyA % copyB;
+
+        copyA = copyB;
+        copyB = tempMod;
+
+        
+    }
+
+    return copyA;
+
+}
