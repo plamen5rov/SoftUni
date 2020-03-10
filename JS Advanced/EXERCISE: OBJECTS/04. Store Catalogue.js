2996a8aa063f2ab3368b2ee634e01e4c0c7bb5d9
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-03-10 20:38:36 +0200

    Create 04. Store Catalogue.js

---
+++ JS Advanced/EXERCISE: OBJECTS/04. Store Catalogue.js
@@ -0,0 +1,50 @@
+//@ts-check
+
+function solve(input) {
+    
+    let catalogue = {};
+
+    const alphaSort = (a, b) => a.localeCompare(b);
+
+    for (let line of input){
+
+        let [name, price] = line.split(" : ");
+        price = Number(price);
+        let initial = name[0];
+
+        if (!catalogue.hasOwnProperty(initial)){
+
+            catalogue[initial] = {};
+
+        }
+
+        let products = catalogue[initial]
+
+
+        if (!products.hasOwnProperty(name)){
+
+            products[name] = price;
+        }
+
+    }
+
+    let sortedInitials = Object.keys(catalogue).sort(alphaSort);
+
+    for (let initial of sortedInitials){
+
+        console.log(initial);
+        
+        let products = catalogue[initial];
+
+        let sortedProducts = Object.keys(products).sort(alphaSort);
+
+        for(let p of sortedProducts){
+
+            console.log(`  ${p}: ${products[p]}`);
+
+        }
+
+    }
+
+}
+    
