edd1187eaeec668ca5ed9c502b3fc9678d0a34a4
Plamen Petrov <51506147+plamen5rov@users.noreply.github.com>
2020-03-10 12:50:26 +0200

    Update 01. Heroic Inventory.js

--- JS Advanced/EXERCISE: OBJECTS/01. Heroic Inventory.js
+++ JS Advanced/EXERCISE: OBJECTS/01. Heroic Inventory.js
@@ -12,3 +12,19 @@ function inventory(input) {
 
     return JSON.stringify(heroes);
 }
+
+//ANOTHER SOLUTION:
+
+function inventory(data){
+
+    return JSON.stringify(data.reduce((acc, heroString, i, arr) => {
+
+        let [name, level, items] = heroString.split(' / ')
+
+        acc.push({name, level: Number(level), items: items ? items.split(',').map(x => x.trim()) : []})
+
+        return acc;
+
+        //return [...acc, {name: heroName, age, inventory}]
+    }, []));
+}
